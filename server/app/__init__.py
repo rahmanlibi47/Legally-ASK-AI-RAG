b2b914c82032ed7f425efb2f7e46fbe0f73d8df9
Libin Rahman <rahmanlibi47@gmail.com>
2025-03-21 12:43:07 +0530

    updating migratoins

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +13,7 @@ def create_app():
     print("Initializing app")
     
     db.init_app(app)
+    migrate.init_app(app, db)
     
     CORS(app)
 
